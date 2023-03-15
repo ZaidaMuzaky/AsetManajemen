7b5801896773ed063604429bc0e7554ffad4adcf
711muzaky <711muzaky@gmail.com>
2023-03-15 08:31:20 +0700

    add login controler, database seeder

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -20,28 +20,28 @@
 			<div class="row d-flex justify-content-center align-item-center">
 				<div class="col-md-12 col-lg-8">
 					<div class="wrap d-md-flex">
-
 						<div class="img d-flex align-items-center">
-              <img src="asset/LogoReka.png"
-          class="img-fluid" alt="Sample image">
-			      </div>
-				  
-						<div class="login-wrap p-4 p-md-5">
-			      	<div class="d-flex">
-			      		<div class="w-100">
-			      			<h3 class="mb-4">Sign In</h3>
+							<img src="asset/LogoReka.png"
+						class="img-fluid" alt="Sample image">
 			      		</div>
+							<div class="login-wrap p-4 p-md-5">
+								<div class="d-flex">
+									<div class="w-100">
+			      					<h3 class="mb-4">Sign In</h3>
+			      					</div>
 								<div class="w-100">
 								</div>
 			      	</div>
-							<form action="#" class="signin-form">
+					
+							<form action="/login" method="post" class="signin-form">
+								@csrf
 			      		<div class="form-group mb-3">
-			      			<label class="label" for="name">Username</label>
-			      			<input type="text" class="form-control" placeholder="Username" required>
+			      			<label class="label" for="email">Email</label>
+			      			<input type="text" name="email" class="form-control" placeholder="Email" required>
 			      		</div>
 		            <div class="form-group mb-3">
 		            	<label class="label" for="password">Password</label>
-		              <input type="password" class="form-control" placeholder="Password" required>
+		              <input type="password" name="password" class="form-control" placeholder="Password" required>
 		            </div>
 		            <div class="form-group">
 		            	<button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign In</button>
